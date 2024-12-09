98689971f1b07d3c65f8a5bfb830b6ddc6f6282e
Anthony Grenier <71669332+Grant2003@users.noreply.github.com>
2024-12-09 18:07:31 -0500

    Update outils.php

--- outils.php
+++ outils.php
@@ -4,23 +4,22 @@
   //   Fichier :  outils.php
   //   Par:      Alain Martel
   //   Date :    2024-10-21
-  //   Modifié par :  
+  //   Modifié par :  Anthony Grenier
   //-----------------------------------
 
 
  
-/*
-define('SERVEUR', 'amartel.techinfo-cstj.ca');
-define('UTILISATEUR', 'amartel');
-define('MOT_DE_PASSE', '');
-define('BD', 'amartel_jv24Synthese');
-*/
+
+define('SERVEUR', '2071623.techinfo-cstj.ca');
+define('UTILISATEUR', 'e2071623_anthonygrenier');
+define('MOT_DE_PASSE', 'Poulet450@p');
+define('BD', 'e2071623_jv-synthese');
 
 
-define('SERVEUR', 'localhost');
-define('UTILISATEUR', 'root');
-define('MOT_DE_PASSE', '');
-define('BD', 'jv24-synthese');
+//define('SERVEUR', 'localhost');
+//define('UTILISATEUR', 'root');
+//define('MOT_DE_PASSE', '');
+//define('BD', 'jv24-synthese');
 
 
 ini_set('date.timezone', 'america/new_york');
